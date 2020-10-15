12013285ddf96cc308668a73d17f70ec99763d3e
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-14 23:11:31 -0600

    now rendering the dealers cards

--- src/Components/BlackJackTable.js
+++ src/Components/BlackJackTable.js
@@ -1,22 +1,49 @@
 import React, { useState, useEffect } from 'react'
+// import Image from '../Images/blackjack-cards/'
 import Cards from '../Utilities/Cards'
 
 export default () => {
   const [ cards, setCards ] = useState(Cards)
-  const [ shuffledCards, setShuffledCards ] = useState()
-  const [ playerCards, setPlayerCards ] = useState();
-  const [ dealerCards, setDealerCards ] = useState();
-  
+  const [ shuffledCards, setShuffledCards ] = useState([])
+  const [ playerCards, setPlayerCards ] = useState([]);
+  const [ dealerCards, setDealerCards ] = useState([]);
+
   useEffect(() => {
     setShuffledCards(cards.sort(() => .5 - Math.random()))
   }, [])
 
-  console.log(shuffledCards)
+  useEffect(() => {
+    if (shuffledCards.length > 0) {
+      setDealerCards([shuffledCards[0], shuffledCards[1]])
+      setPlayerCards([shuffledCards[2], shuffledCards[3]])
+    }
+  }, [shuffledCards])
+
+  // console.log(dealerCards)
+  // console.log(playerCards)
   return (
-    <div style={{ backgroundColor: "#744A35", height: "80%" }}>
-      <div style={{ backgroundColor: "#008000", height: "90%", borderBottomLeftRadius: "50%", borderBottomRightRadius: "50%" }}>
-        <div>Dealer half</div>
-        <div>Player half</div>
+    <div style={{ backgroundColor: "#1D2020", height: "80%" }}>
+      <div style={{ backgroundColor: "#744A35", height: "100%", borderBottomLeftRadius: "50%", borderBottomRightRadius: "50%", borderBottom: "30px solid #744A35" }}>
+        <div style={{ backgroundColor: "#008000", height: "90%", borderBottomLeftRadius: "50%", borderBottomRightRadius: "50%" }}>
+          <div>
+            {
+              dealerCards.length > 0 ? (
+                dealerCards.map((card) => {
+                  return (
+                    <div>
+                      <img
+                        style={{ width: "75px", height: "100px" }}
+                        src={require("../Images/blackjackCards/".concat(card.image))}
+                      />
+                    </div>
+                  )
+                })
+              ) : "Loading..."
+            }
+          </div>
+
+          <div>Player half</div>
+        </div>
       </div>
     </div>
   )
