d766f57856dac3a5f6dbdff950ae6fee3db10823
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-14 21:58:36 -0600

    cleaned up useless code in app component

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,9 +6,6 @@ function App() {
   return (
     <div className="App" style={{ height: "100vh" }}>
       <BlackJackTable />
-      {/* <div style={{ backgroundColor: "#744A35", height: "10%" }}>
-
-      </div> */}
       <BottomPlayerIndicator />
     </div>
   );
