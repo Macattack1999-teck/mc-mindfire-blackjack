183e5cd9769517480f2852e787e44b7df4a104d8
Michael Cortez <CortezDevelopement@gmail.com>
2020-10-15 00:02:40 -0600

    reformatted BottomPlayerIndicator component and added 2 functions that currently do nothing but will soon handle hitting and holding logic

--- src/Components/BottomPlayerIndicator.js
+++ src/Components/BottomPlayerIndicator.js
@@ -1,26 +1,37 @@
-import React, { useContext } from 'react'
-import PointCalculatorContext from '../Contexts/PointCalculatorContext'
+import React, { useContext } from "react";
+import PointCalculatorContext from "../Contexts/PointCalculatorContext";
 
 export default () => {
-  const {
-    dealerPoints,
-    playerPoints
-  } = useContext(PointCalculatorContext)
+  const { dealerPoints, playerPoints } = useContext(PointCalculatorContext);
+
+  const handleHitting = () => {
+    console.log("hitting");
+  };
+
+  const handleHolding = () => {
+    console.log("holding");
+  };
 
   return (
-    <div style={{ height: "20%", backgroundColor: "#1D2020", display: "flex", justifyContent: "space-around", alignItems: "center", color: "#fff", textAlign: "center" }}>
+    <div
+      style={{
+        height: "20%",
+        backgroundColor: "#1D2020",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        color: "#fff",
+        textAlign: "center"
+      }}
+    >
       <div>
         <div style={{ borderBottom: "1px solid #CCC", textAlign: "center" }}>
           Dealer
         </div>
 
-        <div style={{ paddingTop: "5px" }}>
-          Playing
-        </div>
+        <div style={{ paddingTop: "5px" }}>Waiting...</div>
 
-        <div style={{ paddingTop: "5px" }}>
-          Current points: {dealerPoints}
-        </div>
+        <div style={{ paddingTop: "5px" }}>Current points: {dealerPoints}</div>
       </div>
 
       <div>
@@ -28,12 +39,18 @@ export default () => {
           Options
         </div>
 
-        <div style={{ display: "flex", justifyContent: "center", paddingTop: "5px" }}>
-          <div style={{ width: "40px"}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "5px"
+          }}
+        >
+          <div style={{ width: "40px" }} onClick={handleHitting}>
             Hit
           </div>
 
-          <div style={{ width: "40px"}}>
+          <div style={{ width: "40px" }} onClick={handleHolding}>
             Hold
           </div>
         </div>
@@ -44,14 +61,10 @@ export default () => {
           Player
         </div>
 
-        <div style={{ paddingTop: "5px" }}>
-          Waiting
-        </div>
+        <div style={{ paddingTop: "5px" }}>Playing</div>
 
-        <div style={{ paddingTop: "5px" }}>
-          Current points: {playerPoints}
-        </div>
+        <div style={{ paddingTop: "5px" }}>Current points: {playerPoints}</div>
       </div>
     </div>
-  )
-}
+  );
+};
